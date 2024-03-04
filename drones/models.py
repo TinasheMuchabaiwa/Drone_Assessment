d9e0939eccc765b9093b725afe3189cb2aaf85ad
Tinashe Muchabaiwa <tinashe.muchabaiwa@koneksys.com>
2024-03-04 17:41:14 +0200

    Initial definition of the Medication model

--- drones/models.py
+++ drones/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class Drone(models.Model):
@@ -27,3 +28,39 @@ class Drone(models.Model):
 
     def __repr__(self):
         return f"Drone: {self.serial_number}, {self.model}"
+
+
+# Each Medication has:
+
+# name (allowed only letters, numbers, â€˜-â€˜, â€˜_â€™);
+# weight;
+# code (allowed only upper case letters, underscore and numbers);
+# image (picture of the medication case).
+
+
+class Medication(models.Model):
+    name = models.CharField(
+        max_length=100,
+        validators=[
+            RegexValidator(
+                regex='^[a-zA-Z0-9_-]*$',
+                message='Name must be Alphanumeric',
+                code='invalid_name'
+            )
+        ]
+    )
+    weight = models.FloatField()
+    code = models.CharField(
+        max_length=100,
+        validators=[
+            RegexValidator(
+                regex='^[A-Z0-9_]*$',
+                message='upper case letters, underscore, and numbers allowed',
+                code='invalid_code'
+            )
+        ]
+    )
+    image = models.ImageField(upload_to='/medication_images', blank=True)
+
+    def __repr__(self):
+        return f"Medication: {self.name}, {self.code}"
