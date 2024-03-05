f5cd569032b4d139e326233a3d040463e0f4c4e0
Tinashe Muchabaiwa <tinashe.muchabaiwa@koneksys.com>
2024-03-05 19:21:00 +0200

    integrated swaggerui configuration with function serializers

--- Drone_Assessment/drones/serializers.py
+++ Drone_Assessment/drones/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from drones.models import Drone, Medication
+from drf_yasg import openapi
 
 
 class MedicationSerializer(serializers.ModelSerializer):
@@ -8,6 +9,37 @@ class MedicationSerializer(serializers.ModelSerializer):
         fields = (
             'name', 'weight', 'code', 'image'
         )
+    register_medication_request_body = openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'name': openapi.Schema(
+                type=openapi.TYPE_STRING,
+                default='paracetamol'
+            ),
+            'weight': openapi.Schema(
+                type=openapi.TYPE_NUMBER,
+                default=12.0
+            ),
+            'code': openapi.Schema(
+                type=openapi.TYPE_STRING,
+                default='PAR_00123'
+            ),
+            'image': openapi.Schema(
+                type=openapi.TYPE_STRING
+            )
+        },
+        required=['name, weight, code']
+    )
+    responses = {
+        'register_medication': {
+            201: openapi.Response(
+                description="Drone registered successfully",
+            ),
+            400: "Input validation failed",
+            409: "Drone with this serial number already exists",
+            500: "Internal Server Error"
+        }
+    }
 
     def create(self, validated_data):
         medication = Medication.objects.create(
@@ -25,6 +57,38 @@ class DroneSerializer(serializers.ModelSerializer):
             'serial_number', 'model', 'weight_limit',
             'battery_capacity', 'medications', 'current_medication_weight'
         )
+    register_drone_request_body = openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'serial_number': openapi.Schema(
+                type=openapi.TYPE_STRING,
+                default='SBGH00123'
+            ),
+            'model': openapi.Schema(
+                type=openapi.TYPE_STRING,
+                default='Lightweight'
+            ),
+            'weight_limit': openapi.Schema(
+                type=openapi.TYPE_NUMBER,
+                default=500.0
+            ),
+            'battery_capacity': openapi.Schema(
+                type=openapi.TYPE_NUMBER,
+                default=100
+            ),
+        },
+        required=['serial_number', 'model', 'weight_limit', 'battery_capacity']
+    )
+    responses = {
+        'register_drone': {
+            201: openapi.Response(
+                description="Drone registered successfully",
+            ),
+            400: "Input validation failed",
+            409: "Drone with this serial number already exists",
+            500: "Internal Server Error"
+        }
+    }
 
     def create(self, validated_data):
         if 'medications' in validated_data:
