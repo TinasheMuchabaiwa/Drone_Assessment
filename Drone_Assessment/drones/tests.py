2c70acadfb12d2237e6c551797db264aac689d9a
Tinashe Muchabaiwa <tinashe.muchabaiwa@koneksys.com>
2024-03-05 21:11:05 +0200

    tests for loading drone with medications

--- Drone_Assessment/drones/tests.py
+++ Drone_Assessment/drones/tests.py
@@ -68,3 +68,90 @@ class DroneTests(APITestCase):
             response.data['errors']['serial_number'],
             ['drone with this serial number already exists.']
         )
+
+    def test_load_drone_with_medication(self):
+        # Create a drone
+        drone_url = reverse('register-drone')
+        drone_data = {
+            'serial_number': 'XXXXXX12345',
+            'model': 'Lightweight',
+            'battery_capacity': 100,
+            'state': 'IDLE',
+            'weight_limit': 500,
+        }
+        drone_response = self.client.post(drone_url, drone_data, format='json')
+        self.assertEqual(drone_response.status_code, status.HTTP_201_CREATED)
+        drone_id = 1
+
+        # Create a medication
+        medication_url = reverse('register-medication')
+        medication_data = {
+            'name': 'paracetamol',
+            'weight': 12.0,
+            'code': 'PARR_00123',
+        }
+        medication_response = self.client.post(
+            medication_url,
+            medication_data,
+            format='json'
+        )
+        self.assertEqual(
+            medication_response.status_code,
+            status.HTTP_201_CREATED
+        )
+        medication_id = 1
+
+        # Load drone with medication
+        url = reverse(
+            'load-drone-with-medication',
+            kwargs={'drone_id': drone_id}
+        )
+        data = {
+            'medications': [medication_id],
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Drone.objects.get().current_medication_weight, 12.0)
+
+    def test_load_drone_with_medication_drone_not_found(self):
+        # Create a drone
+        drone_url = reverse('register-drone')
+        drone_data = {
+            'serial_number': 'XXXXXX12345',
+            'model': 'Lightweight',
+            'battery_capacity': 100,
+            'state': 'IDLE',
+            'weight_limit': 500,
+        }
+        drone_response = self.client.post(drone_url, drone_data, format='json')
+        self.assertEqual(drone_response.status_code, status.HTTP_201_CREATED)
+        drone_id = 2
+
+        # Create a medication
+        medication_url = reverse('register-medication')
+        medication_data = {
+            'name': 'paracetamol',
+            'weight': 12.0,
+            'code': 'PARR_00123',
+        }
+        medication_response = self.client.post(
+            medication_url,
+            medication_data,
+            format='json'
+        )
+        self.assertEqual(
+            medication_response.status_code,
+            status.HTTP_201_CREATED
+        )
+        medication_id = 1
+
+        # Load drone with medication
+        url = reverse(
+            'load-drone-with-medication',
+            kwargs={'drone_id': drone_id}
+        )
+        data = {
+            'medications': [medication_id],
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
